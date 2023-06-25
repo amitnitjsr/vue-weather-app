609c01f538886cafa6ad6f26e79fa02370d074bd
amitnitjsr <amitnips007@gmail.com>
2023-06-25 17:02:09 +0530

    modal addded

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>
+    <header class="stricky bg-weather-primary shadow-lg">
+        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+            <RouterLink :to="{name: 'home'}">
+            <div class="flex items-center gap-3 ">
+                <i class="fa-solid fa-sun text-2xl"></i>
+                <p class="text-2xl">The Local Weather</p>
+            </div>
+            </RouterLink>
 
-    </div>
+            <div class="flex gap-3 flex-1 justify-end">
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+            </div>
+
+            <BaseModal>
+
+            </BaseModal>
+        </nav>
+    </header>
 </template>
 
 <script setup>
+import { RouterLink } from 'vue-router';
+import BaseModal './BaseModal.vue'
 
 </script>
 
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+const { createRouter, createWebHistory } = require("vue-router");
+import HomeView from '../views/HomeView.js'
+
+const router = createRouter({
+    history: createWebHistory
+    (import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: HomeView
+        },
+    ]
+});
+
+export default router;
